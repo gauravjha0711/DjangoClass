9bd7c734c3b57d2fff5604f571abb9099e3cb04b
gauravjha0711 <gauravjha0711@gmail.com>
2025-08-01 10:39:26 +0530

    update day8_1, make dictionary

--- day8_1/myapp/urls.py
+++ day8_1/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('home/',views.hellofunction)
+    path('home/',views.hellofunction),
+    path('mydict/',views.mydict),
 ]
--- day8_1/myapp/views.py
+++ day8_1/myapp/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hellofunction(request):
-    return HttpResponse("Hello, World!")
+    # return HttpResponse("Hello, World!")
+    # return HttpResponse("<h1>Hello, World!</h1>")
+    name = "Gaurav Jha"
+    place = "Patna"
+    # return HttpResponse("My name is "+name+" and I am from "+place+".")
+    # return HttpResponse(f"My name is {name} and I am from {place}.")
+    a=10
+    b=20
+    return HttpResponse(f"The sum of {a} and {b} is {a+b}.")
+
+def mydict(request):
+    items = {
+        "a" : "about a",
+        "b" : "about b",
+        "c" : "about c",
+    }
+    context = "Menu Items:<br>"
+    for key, value in items.items():
+        context+= f"<li>{key} : {value}</li>"
+    return HttpResponse(context)
