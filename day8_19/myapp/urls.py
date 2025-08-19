ee24929e036aeac03fba9b85aee5c1b46d16f1a0
gauravjha0711 <gauravjha0711@gmail.com>
2025-08-19 12:59:37 +0530

    day8_19 template

---
+++ day8_19/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,re_path
+from . import views
+urlpatterns = [
+    # path('json/',views.json),
+    # path('apidata/',views.apidata),
+    # re_path(r'^regex/(?P<username>[a-zA-Z]+)/$', views.regex),
+
+    
+    path('indexpage/',views.indexpage),
+]
---
+++ day8_19/myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import requests
+# Create your views here.
+# def json(request):
+#     data = {
+#         "name" : "Gaurav",
+#         "age" : 20,
+#         "city" : "Patna"
+#     }
+#     return JsonResponse(data)
+
+# def apidata(request):
+#     response = requests.get("https://api.sampleapis.com/futurama/characters")
+#     data = response.json()
+#     return JsonResponse(data, safe=False)
+#     # return HttpResponse(response)
+
+# def regex(request,username):
+#     return HttpResponse(f"Hello {username}")
+
+def indexpage(request):
+    return render(request,'myapp/index.html')
