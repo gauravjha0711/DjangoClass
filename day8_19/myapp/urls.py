7923fa17bfe38c4d884766b688be0fe16e051305
gauravjha0711 <gauravjha0711@gmail.com>
2025-08-19 14:00:08 +0530

    include added

--- day8_19/myapp/urls.py
+++ day8_19/myapp/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path,re_path
 from . import views
 urlpatterns = [
-    # path('json/',views.json),
-    # path('apidata/',views.apidata),
-    # re_path(r'^regex/(?P<username>[a-zA-Z]+)/$', views.regex),
-
     
     path('indexpage/',views.indexpage),
+    path('abc/',views.abc),
 ]
