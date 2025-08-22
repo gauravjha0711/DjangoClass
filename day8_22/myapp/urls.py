dc115026b97a7bf080df82acf6522b40235abd84
gauravjha0711 <gauravjha0711@gmail.com>
2025-08-22 09:42:23 +0530

    day8_22 created

---
+++ day8_22/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,re_path
+from . import views
+urlpatterns = [
+    
+    path('indexpage/',views.indexpage),
+    path('abc/',views.abc),
+    path('new/',views.new),
+]
---
+++ day8_22/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import requests
+# Create your views here.
+
+def indexpage(request):
+    return render(request,'myapp/child.html')
+def new(request):
+    return render(request,'myapp/new.html')
+def abc(request):
+    return render(request,'myapp/abc.html')
