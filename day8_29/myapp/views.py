9d9f9791de0c793d1b7c19c4e9c380783a125e22
gauravjha0711 <gauravjha0711@gmail.com>
2025-08-29 12:19:33 +0530

    day8_29

---
+++ day8_29/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import requests
+# Create your views here.
+
+def indexpage(request):
+    contex = {
+        "username": "Gaurav",
+        "age": 25,
+        "location": "patna, Bihar",
+        'mylist': [1,2,3,4,5,6],
+    }
+    return render(request,'myapp/child.html',contex)
+def new(request):
+    return render(request,'myapp/new.html')
+def abc(request):
+    return render(request,'myapp/abc.html')
